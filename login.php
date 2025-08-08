9ea97c98e0f96aa3395e5bf9e95936947388f2f8
TechMaster2099 <127751338+TechMaster2099@users.noreply.github.com>
2025-08-08 11:41:35 +0300

    Added log In

---
+++ login.php
@@ -0,0 +1,26 @@
+<?php
+ 
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>log in </title>
+</head>
+<body>
+    <h2>Log In</h2>
+    <form>
+        <label>username:</label>
+        <br>
+        <input type="text" name="username" required>
+        <br><br>
+        <label>Password</label>
+        <br>
+        <input type="password" name="password" required>
+        <br><br>
+        <button type="submit">Log In</button>
+
+    </form>
+</body>
+</html>
